317ed14f17a0fdb8e061af98b118a74ebdf56e77
Diego Ferreiro Val <elfogris@gmail.com>
2014-12-31 02:50:14 -0800

    fixing aura grunt again

--- GruntFile.js
+++ GruntFile.js
@@ -16,21 +16,21 @@
 
 var COPYRIGHT = [
     '/*',
-     '* Copyright (C) 2013 salesforce.com, inc.',
-     '*',
-     '* Licensed under the Apache License, Version 2.0 (the "License");',
-     '* you may not use this file except in compliance with the License.',
-     '* You may obtain a copy of the License at',
-     '*',
-     '*         http://www.apache.org/licenses/LICENSE-2.0',
-     '*',
-     '* Unless required by applicable law or agreed to in writing, software',
-     '* distributed under the License is distributed on an "AS IS" BASIS,',
-     '* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.',
-     '* See the License for the specific language governing permissions and',
-     '* limitations under the License.',
-     '*/',
-     ''
+    ' * Copyright (C) 2013 salesforce.com, inc.',
+    ' *',
+    ' * Licensed under the Apache License, Version 2.0 (the "License");',
+    ' * you may not use this file except in compliance with the License.',
+    ' * You may obtain a copy of the License at',
+    ' *',
+    ' *         http://www.apache.org/licenses/LICENSE-2.0',
+    ' *',
+    ' * Unless required by applicable law or agreed to in writing, software',
+    ' * distributed under the License is distributed on an "AS IS" BASIS,',
+    ' * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.',
+    ' * See the License for the specific language governing permissions and',
+    ' * limitations under the License.',
+    ' */',
+    '',''
 ].join('\n');
 
 module.exports = function (grunt) {
@@ -73,7 +73,10 @@ module.exports = function (grunt) {
                     {src: 'src/surface-manager.js',           dest: 'build/aura/scrollerLib/SurfaceManager.js'},
                     {src: 'src/plugins/pull-to-refresh.js',   dest: 'build/aura/scrollerLib/PullToRefresh.js'},
                     {src: 'src/plugins/pull-to-load-more.js', dest: 'build/aura/scrollerLib/PullToLoadMore.js'},
-                    {src: 'src/plugins/infinite-loading.js',  dest: 'build/aura/scrollerLib/InfiniteLoading.js'}
+                    {src: 'src/plugins/infinite-loading.js',  dest: 'build/aura/scrollerLib/InfiniteLoading.js'},
+                    {src: 'src/plugins/endless-plugin.js',    dest: 'build/aura/scrollerLib/EndlessPlugin.js'},
+                    {src: 'src/plugins/indicators-plugin.js', dest: 'build/aura/scrollerLib/IndicatorsPlugin.js'},
+                    {src: 'src/plugins/snap-plugin.js',       dest: 'build/aura/scrollerLib/SnapPlugin.js'}
                 ],
                 options: {
                     process: function (content, path) {
